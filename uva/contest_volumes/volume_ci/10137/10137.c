da7fff6c14721d7c73c977de74f427680a06e9d2
tnaires <tnaires@gmail.com>
2011-07-24 16:36:46 -0300

    First version of solution for UVA problem 10137.

--- uva/contest_volumes/volume_ci/10137/10137.c
+++ uva/contest_volumes/volume_ci/10137/10137.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
+	int n;
+	float spending[1000];
+	
+	while (scanf("%d", &n) != EOF) {
+		if (n == 0) break;
+		
+		int i;
+		float sum = 0;
+		
+		for (i = 1; i <= n; i++) {
+			scanf("%f", &spending[i]);
+			sum += spending[i];
+		}
+		
+		float avg = ceil((sum / n) * 100) / 100;
+		float result = 0;
+		
+		for (i = 1; i <= n; i++) {
+			if (spending[i] > avg) {
+				result += spending[i] - avg;
+			}
+		}
+		
+		printf("$%.2f\n", result);
+	}
+	
 	return 0;
 }
