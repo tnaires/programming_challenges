e04985107c1be9e34a797257752dbb00aa45f10c
tnaires <tnaires@gmail.com>
2011-07-24 18:29:03 -0300

    Problem 10137 (UVA) accepted.

--- uva/contest_volumes/volume_ci/10137/10137.c
+++ uva/contest_volumes/volume_ci/10137/10137.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 	int n;
-	float spending[1000];
+	double spending[1000];
 	
 	while (scanf("%d", &n) != EOF) {
 		if (n == 0) break;
 		
 		int i;
-		float sum = 0;
+		double sum = 0;
 		
 		for (i = 1; i <= n; i++) {
-			scanf("%f", &spending[i]);
+			scanf("%lf", &spending[i]);
 			sum += spending[i];
 		}
 		
-		float avg = ceil((sum / n) * 100) / 100;
-		float result = 0;
+		double avg = sum / n;
+		avg = (long) (avg * 100 + 0.5);
+		avg /= 100;
+		
+		double result_over = 0, result_under = 0;
 		
 		for (i = 1; i <= n; i++) {
 			if (spending[i] > avg) {
-				result += spending[i] - avg;
+				result_over += spending[i] - avg;
+			} else if (spending[i] < avg) {
+				result_under += avg - spending[i];
 			}
 		}
 		
-		printf("$%.2f\n", result);
+		printf("$%.2lf\n", result_over < result_under ? result_over : result_under);
 	}
 	
 	return 0;
